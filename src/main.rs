13cc46aea5ef4f9f15ca73e86bbc29396ba68895
Dylan Landry <dylan.landry@milkmoovement.com>
2022-07-10 12:16:09 -0400

    add basic tui-rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,52 @@
+// Stopwatch deps
 use std::{
     collections::HashMap,
     thread,
     time::{self, Duration, SystemTime},
 };
 
-fn main() {
-    let mut app = App::new();
-    app.start();
+// tui-rs deps
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io;
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders, Widget},
+    Terminal,
+};
 
-    thread::sleep(time::Duration::from_secs(1));
-    println!(
-        "Focused for {} seconds!",
-        app.get_current_session_duration().unwrap().as_secs()
-    );
+fn main() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-    app.pause();
-    thread::sleep(time::Duration::from_secs(1));
-    app.start();
-    thread::sleep(time::Duration::from_secs(4));
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Focus Stopwatch")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
 
-    println!(
-        "Focused for {} seconds!",
-        app.get_current_session_duration().unwrap().as_secs()
-    );
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
 
 struct App {
